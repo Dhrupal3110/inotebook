ead4df77cabecaadebf0aaca056259fc7780176d
dhrupal rafaliya <dhrupalrafaliya2001@gmail.com>
2022-02-15 08:14:53 +0530

    Creating component for react set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+import Home from './component/Home';
+import Navbar from "./component/NavBar";
+import About from "./component/About"
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <BrowserRouter>
+      <Navbar/>
+        <Routes>
+         
+          <Route exact path="/" element={<Home/>} />
+          <Route exact path="/about" element={<About/>} />
+
+        </Routes>
+      </BrowserRouter>
+    </>
   );
-}
+};
 
 export default App;
